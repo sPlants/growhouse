d3c4c766cf64e136ea9dfe2fa9570ceee28ca1f0
unknown <simontoftbruun@hotmail.com>
2015-12-31 13:12:17 +0100

    initial commit, stabel MEAN environment.

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,26 @@
+var app = angular.module('growhouseApp', ['ui.router'])
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/views/home.ejs',
+      controller: 'MainCtrl',
+    })
+  $urlRouterProvider.otherwise('home');
+}]);
+
+app.controller('MainCtrl', [
+'$scope',
+function($scope){
+
+  $scope.test = 'Hello world!';
+
+//End MainCtrl
+  }
+]);
+
